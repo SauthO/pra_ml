ed66cbb5fd78c63b0eae56f36ed810a97fcd040e
SauthO <shuta0698@gmail.com>
2024-04-24 22:26:22 +0900

    train_test_split 20240424

--- pra_ml/fetch_data.py
+++ pra_ml/fetch_data.py
@@ -1,6 +1,9 @@
 import os
 import tarfile
 import urllib.request
+from zlib import crc32
+import pandas as pd
+import numpy as np
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -15,3 +18,22 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+def split_train_set(df, test_ratio):
+    shuffled_indices = np.random.permutation(len(df))
+    test_set_size = int(len(df) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return df.iloc[train_indices], df.iloc[test_indices] # iloc is Pandas method
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(df, test_ratio, id_column):
+    ids = df[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return df.loc[~in_test_set], df.loc[in_test_set]
--- tests/fetch_data_test.py
+++ tests/fetch_data_test.py
@@ -1,33 +1,21 @@
-from pra_ml import fetch_data as fd
-import pandas as pd
-import os
-import matplotlib.pyplot as plt
-import numpy as np
+from pra_ml import fetch_data
+from sklearn.model_selection import train_test_split
 
-HOUSING_PATH = os.path.join("datasets", "housing")
-
-def load_housing_data(housing_path=HOUSING_PATH):
-    csv_path = os.path.join(housing_path, "housing.csv")
-    return pd.read_csv(csv_path)
-
-#fd.fetch_housing_data()
-
-
-housing = load_housing_data()
+housing = fetch_data.load_housing_data()
 print(housing.head())
 print(housing.info())
 print(housing["ocean_proximity"].value_counts())
 print(housing.describe())
 
-def split_train_set(df, test_ratio):
-    shuffled_indices = np.random.permutation(len(df))
-    test_set_size = int(len(df) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return df.iloc[train_indices], df.iloc[test_indices] # iloc is Pandas method
-
-train_set, test_set = split_train_set(housing, 0.2)
+train_set, test_set = fetch_data.split_train_set(housing, 0.2)
 print(len(train_set))
 print(len(test_set))
+
+housing_with_id = housing.reset_index() # add index row
+print(housing_with_id)
+train_set, test_set = fetch_data.split_train_test_by_id(housing_with_id, 0.2, "index")
+
+train_set, test_set = train_test_split(housing_with_id, test_size=0.2, random_state=42)
+
 housing.hist(bins=50, figsize=(20, 15))
 #plt.show()
